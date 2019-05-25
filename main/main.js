56fb199a14dd3bf1d0d59eb27fddca363354890d
xiaoming <15xmchen@stu.edu.cn>
2019-05-25 20:19:51 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,33 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputStr) {
+    // 数字符号映射表
+    var numSign = {
+        '0': ['._.', '|.|', '|_|'],
+        '1': ['...', '..|', '..|'],
+        '2': ['._.', '._|', '|_.'],
+        '3': ['._.', '._|', '._|'],
+        '4': ['...', '|_|', '..|'],
+        '5': ['._.', '|_.', '._|'],
+        '6': ['._.', '|_.', '|_|'],
+        '7': ['._.', '..|', '..|'],
+        '8': ['._.', '|_|', '|_|'],
+        '9': ['._.', '|_|', '..|']
+    };
+
+    const LINE = 3;    // 分三行扫描输出
+    var outputStr = '';
+    var len = inputStr.length;
+
+    for (var i = 0; i < LINE; i++) {
+        for (var j = 0; j < len; j++) {
+            outputStr += numSign[inputStr[j]][i];
+            if (j != len - 1) {    // 每行最后一个符号不加空格符，其余均加空格符
+                outputStr += ' ';
+            }
+        }
+        outputStr += '\n';
+    }
+
+    console.log(outputStr);
+
+    return outputStr;
 };
